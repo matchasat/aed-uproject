9459b5aaf9fe8e2c09dd699b88ee8294cff7a4c8
Kiran0517 <145093418+Kiran0517@users.noreply.github.com>
2023-12-04 22:54:16 -0500

    Add files via upload

---
+++ assignment-3_AED/Assignment-3_AED/src/Business/AdminPackage/Semester.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.AdminPackage;
+
+/**
+ *
+ * @author asus
+ 
+ */
+public class Semester {
+    private static String semester;
+
+    public static String getSemester() {
+        return semester;
+    }
+
+    public static void setSemester(String semester) {
+        Semester.semester = semester;
+    }
+    
+    
+    
+}
